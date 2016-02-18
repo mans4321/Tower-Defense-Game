cbcbf9e5b9a6132262942d10d5b10dd1f27f4f33
Sandy <Sandy@DESKTOP-9FRL8RG.wireless.concordia.ca>
2016-02-17 23:13:39 -0500

    adding javadoc

--- src/gamemodel/critter/StealCoinListener.java
+++ src/gamemodel/critter/StealCoinListener.java
@@ -1,14 +1,20 @@
 package gamemodel.critter;
 /**
  * 
- * 
+ * Interface responsible for Compensate the player for the killed critter.
+ *  
  *@author yongpinggao 
  *@since 2/7/16.
  *@version 1.0
  */
 public interface StealCoinListener {
-
+/**
+ *  Compensate the player for the killed critter
+ */
     void coinGotStolen();
-
+/**
+ * 
+ * no critter left to Compensate the player for the killed critter. 
+ */
     void noCritterLeft();
 }
