651d7d4aaa2d35ade57f5333a983856f58458ad5
Sandy <Sandy@DESKTOP-9FRL8RG.wireless.concordia.ca>
2016-02-18 09:56:07 -0500

    adding unittest

---
+++ UnitTesting/unitTestpackage/validatemap.java
@@ -0,0 +1,57 @@
+package unitTestpackage;
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+
+import org.junit.Test;
+
+import gamemodel.gamemap.CellState;
+import mapvalidation.NoEntranceNoExitMoreEntranceMoreExitValidator;
+import mapvalidation.SeperateEntranceAndExitValidator;
+
+public class validatemap {
+
+	
+	@Test
+	public void testNoEnEX() {
+		
+		ArrayList<CellState> cellListtest= new ArrayList<CellState>();
+		
+		for (int i = 0; i < 20; i++) {
+			
+			cellListtest.add(CellState.GRASS );}
+		
+		NoEntranceNoExitMoreEntranceMoreExitValidator  NEE = new NoEntranceNoExitMoreEntranceMoreExitValidator(cellListtest);
+		
+		boolean Re = NEE.validate();
+		
+
+		assertFalse(Re);
+		
+	
+		
+		}
+		
+	@Test
+	public void testToshort() {
+		
+ArrayList<CellState> cellListtest= new ArrayList<CellState>();
+		
+		for (int i = 0; i < 20; i++) {
+			
+			cellListtest.add(CellState.GRASS );}
+		
+		cellListtest.set(0,CellState.ENTRANCE);
+		cellListtest.set(19,CellState.EXIT);
+		
+		SeperateEntranceAndExitValidator SENEX = new SeperateEntranceAndExitValidator(10,cellListtest);
+		
+		boolean Re = SENEX.validate();
+		
+		assertFalse(Re);
+	}
+	
+
+
+}
