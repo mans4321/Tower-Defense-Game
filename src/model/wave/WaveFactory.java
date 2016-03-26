e5efa7136bec5ef005edbd3281b71c5f49cbb082
M_lzahra <M_lzahra@FRENCH-GUIANA.ENCS.concordia.ca>
2016-03-26 14:07:49 -0400

    factory

--- src/model/wave/WaveFactory.java
+++ src/model/wave/WaveFactory.java
@@ -15,17 +15,17 @@ public class WaveFactory {
     public WaveBuilder getWave(int waveNum) {
         switch (waveNum) {
             case 1:
-                return new WaveBuilder().critterA(1).critterB(2).critterC(3);
+                return new WaveBuilder(waveNum).critterA(1).critterB(2).critterC(3);
             case 2:
-                return new WaveBuilder().critterA(1).critterB(1).critterC(1);
+                return new WaveBuilder(waveNum).critterA(1).critterB(1).critterC(1);
             case 3:
-                return new WaveBuilder().critterA(3).critterB(10).critterC(8).critterD(8);
+                return new WaveBuilder(waveNum).critterA(3).critterB(10).critterC(8).critterD(8);
             case 4:
-                return new WaveBuilder().critterA(11).critterB(10).critterC(8).critterD(8);
+                return new WaveBuilder(waveNum).critterA(11).critterB(10).critterC(8).critterD(8);
             case 5:
-                return new WaveBuilder().critterA(13).critterB(10).critterC(8).critterD(8);
+                return new WaveBuilder(waveNum).critterA(13).critterB(10).critterC(8).critterD(8);
             case 6:
-                return new WaveBuilder().critterA(15).critterB(20).critterC(12).critterD(13);
+                return new WaveBuilder(waveNum).critterA(15).critterB(20).critterC(12).critterD(13);
             default:
                 return null;
         }
