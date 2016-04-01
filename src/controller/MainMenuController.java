d01a9ef82f8c38ec57838b3421c251fc23049780
changchanglccc <lichong06091992@gmail.com>
2016-04-01 16:38:38 -0400

    test git

--- src/controller/MainMenuController.java
+++ src/controller/MainMenuController.java
@@ -59,7 +59,6 @@ public class MainMenuController {
 				try {
 					new GameChooseController();
 				} catch (Exception e1) {
-					// TODO Auto-generated catch block
 					e1.printStackTrace();
 				}
 				
