de472c7e9ac5fd2f1039ff2289d4839a4cc0bfa7
m_lzahra <m_lzahra@RWANDA.ENCS.concordia.ca>
2016-04-05 20:21:52 -0400

    critter view java doc

--- src/view/critter/CritterType.java
+++ src/view/critter/CritterType.java
@@ -1,7 +1,10 @@
 package view.critter;
 
 /**
- * Created by yongpinggao on 3/19/16.
+ * set constants for critters  
+ * @author yongpinggao 
+ * @since 3/19/16.
+ * @version 1.0
  */
 public enum CritterType {
     CritterA("critterA"),
@@ -10,11 +13,17 @@ public enum CritterType {
     CritterD("critterD");
 
     private String critterImageName;
-
+    /**
+     * Constructor for CritterType
+     * @param critterName critter name 
+     */
     CritterType(String critterName) {
         this.critterImageName = critterName;
     }
-
+    /**
+     * getter for critter constant
+     * @return critter constant
+     */
     public String getCritterImageName() {
         return critterImageName;
     }
