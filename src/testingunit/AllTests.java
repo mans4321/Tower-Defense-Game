2ca02f41a4b8c5b1a4db465c4379312617fd0f63
m_lzahra <m_lzahra@CROWE.ENCS.concordia.ca>
2016-04-06 18:05:58 -0400

    code stand

--- src/testingunit/AllTests.java
+++ src/testingunit/AllTests.java
@@ -1,4 +1,4 @@
-package testingUnit;
+package testingunit;
 
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
--- src/testingunit/IceTowerShootingBehaviorTest.java
+++ src/testingunit/IceTowerShootingBehaviorTest.java
@@ -1,5 +1,5 @@
 
-package testingUnit;
+package testingunit;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
